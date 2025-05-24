c4e6b1fd018d2a3ff910a52a23b75ab3cae0fc81
ryoqn <allmixedup69music@gmail.com>
2025-05-24 14:22:52 +0900

    dummy commit

--- apps/ui/src/app/home/page.tsx
+++ apps/ui/src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
   auth.user?.displayName
   return !auth.loading ? (
     <div>
-      <h1>Your Profile!</h1>
+      <h1>Your Profile</h1>
       <p>{auth.user?.displayName}</p>
       <p>{auth.user?.email}</p>
       <p>{auth.user?.phoneNumber}</p>
